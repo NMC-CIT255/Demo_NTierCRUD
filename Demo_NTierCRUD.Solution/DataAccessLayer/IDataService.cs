1591f91e1ed7f1f9099ba286d0263a6367250d80
John Velis <john@johnvelis.com>
2018-10-01 13:48:54 -0400

    Add project files.

---
+++ Demo_NTierCRUD.Solution/DataAccessLayer/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Demo_NTierCRUD
+{
+    public interface IDataService
+    {
+        IEnumerable<Character> ReadAll();
+        void WriteAll(IEnumerable<Character> characters);
+    }
+}
